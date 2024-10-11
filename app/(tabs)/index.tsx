aa46ce4e076346bcb83169fad8247b609cbe8682
tony chen <tony.zq.chen@gmail.com>
2024-10-11 18:54:20 +0800

    feat: init by expo

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -4,6 +4,7 @@ import { HelloWave } from '@/components/HelloWave';
 import ParallaxScrollView from '@/components/ParallaxScrollView';
 import { ThemedText } from '@/components/ThemedText';
 import { ThemedView } from '@/components/ThemedView';
+import Echarts from '@/components/Echarts';
 
 export default function HomeScreen() {
   return (
@@ -15,6 +16,7 @@ export default function HomeScreen() {
           style={styles.reactLogo}
         />
       }>
+      <Echarts />
       <ThemedView style={styles.titleContainer}>
         <ThemedText type="title">Welcome!</ThemedText>
         <HelloWave />
